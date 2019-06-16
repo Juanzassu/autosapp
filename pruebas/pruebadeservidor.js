7fd5ad812e3a25fe2319491f2b84cae5abe8c62b
Juanzassu <banano_zassu@hotmail.com>
2019-06-16 00:21:58 -0500

    Se envia archivo ya modificado para correr el servidor

--- pruebas/pruebadeservidor.js
+++ pruebas/pruebadeservidor.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome Node.js');
+  res.end('Kiubo Yisus ya ponte a estudiar y por cierto el burro se la come');
 }).listen(3001, "207.154.246.93");
 console.log('Server running at http://207.154.246.93:3001/');
